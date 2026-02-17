ccd0e20a5c4daa0acee6352c608d5b2814a91828
Zaim-Nilgiriwala <Zaimnilg@gmail.com>
2026-02-17 14:31:06 -0600

    added map

---
+++ mobile/app/(tabs)/index.tsx
@@ -0,0 +1,27 @@
+import { StyleSheet, View } from "react-native";
+import MapView, { Marker } from "react-native-maps";
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <MapView
+        style={StyleSheet.absoluteFill}
+        initialRegion={{
+          latitude: 32.6025,
+          longitude: -85.4808,
+          latitudeDelta: 0.05,
+          longitudeDelta: 0.05,
+        }}
+      >
+        <Marker
+          coordinate={{ latitude: 32.6025, longitude: -85.4808 }}
+          title="Auburn University"
+        />
+      </MapView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: { flex: 1 },
+});
